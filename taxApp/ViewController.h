5c582c9f1bf6ca4669cf9e7de350d941925bd29d
Steven Yang <steven_yang9190@hotmail.com>
2016-01-06 14:29:26 -0600

    Initial Commit

---
+++ taxApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  taxApp
+//
+//  Created by Steven Yang on 1/6/16.
+//  Copyright © 2016 Steven Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ taxApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  taxApp
+//
+//  Created by Steven Yang on 1/6/16.
+//  Copyright © 2016 Steven Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
