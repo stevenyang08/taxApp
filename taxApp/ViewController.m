991996b2f27f40d1eb35ba6e82d86a440a730259
Steven Yang <stevenyang@Stevens-MacBook-Pro.local>
2016-01-07 00:25:40 -0600

    first commit

--- taxApp/ViewController.m
+++ taxApp/ViewController.m
@@ -9,19 +9,70 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UIButton *onCalculateButtonTapped;
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UILabel *taxRateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *taxRate;
+
+
 
 @end
 
 @implementation ViewController
+@synthesize resultLabel, priceTextField;
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.caTax = 0.075;
+    self.chiTax = 0.0925;
+    self.nyTax = 0.045;
+    NSString *percent = @"%";
+    if(self.segmentedControl.selectedSegmentIndex == 0) {
+        double caTaxPercent = 100 * self.caTax;
+        self.taxRate.text = [NSString stringWithFormat:@"%.2f" @"%@", caTaxPercent, percent];
+    }
+    else if(self.segmentedControl.selectedSegmentIndex == 1) {
+        double chiTaxPercent = 100 * self.chiTax;
+        self.taxRate.text = [NSString stringWithFormat:@"%.2f" @"%@", chiTaxPercent, percent];
+    }
+    else if(self.segmentedControl.selectedSegmentIndex == 2) {
+        double nyTaxPercent = 100 * self.nyTax;
+        self.taxRate.text = [NSString stringWithFormat:@"%.2f" @"%@", nyTaxPercent, percent];
+    }
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)calculate:(id)sender {
+
+    NSString *result = priceTextField.text;
+    int taxFieldNum = result.intValue;
+    
+    if(self.segmentedControl.selectedSegmentIndex == 0) {
+        double caResult = taxFieldNum * self.caTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", caResult];
+    }
+    else if(self.segmentedControl.selectedSegmentIndex == 1) {
+        
+        double chiResult = taxFieldNum * self.chiTax;
+        NSString* chiTaxResult = [NSString stringWithFormat:@"%.2f", chiResult];
+        resultLabel.text = chiTaxResult;
+    }
+    else if(self.segmentedControl.selectedSegmentIndex == 2) {
+        
+        double nyResult = taxFieldNum * self.nyTax;
+        NSString* nyTaxResult = [NSString stringWithFormat:@"%.2f", nyResult];
+        resultLabel.text = nyTaxResult;
+    }
+    
 }
+
+
 
 @end
